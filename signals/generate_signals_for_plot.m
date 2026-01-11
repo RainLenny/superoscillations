a26bad568636467870d3e51444acabf5e8eb3ae7
RainLenny <40518324+RainLenny@users.noreply.github.com>
2026-01-11 16:29:21 +0200

    fixed generate signals, added plot

--- signals/generate_signals_for_plot.m
+++ signals/generate_signals_for_plot.m
@@ -1,4 +1,4 @@
-function [SO_signal, Cos_signal,angular_freqs_SO, angular_freqs_COS] = generate_signals_Baranov_2014(freq_scaling,amp_scaling)
+function [SO_signal, Cos_signal,angular_freqs_SO, angular_freqs_COS] = generate_signals_for_plot(freq_scaling,amp_scaling)
 
 % Parameters
 angular_freqs_SO = [1,2,3,4,5] * 0.18 * freq_scaling;
@@ -14,12 +14,11 @@ angular_freqs_COS = 0.9 * freq_scaling; %angular_freqs_SO(end);
 t_0 = 250;
 T = 100;
 
-Cos_normalization = 4.4781551283 * amp_scaling;
-
+Cos_normalization = 4.4781551283; %Scaling such that the energy of the actual drives would be the same
 
 % Function handle for cosine
-Cos_signal = @(t)   -0.5*conj(Cos_normalization .* exp(1i * angular_freqs_COS .* t).*exp(-(t-t_0).^2./T^2));
+Cos_signal = @(t)   -real(Cos_normalization .* cos(angular_freqs_COS .* t).*exp(-(t-t_0).^2./T^2));
 
 % Function handle for superoscillatory signal
-SO_signal = @(t)  0.5*conj(sum( amps_SO .* exp(1i * angular_freqs_SO .* t),2).*exp(-(t-t_0).^2./T^2));
+SO_signal = @(t)  real(sum( amps_SO .* exp(1i * angular_freqs_SO .* t),2).*exp(-(t-t_0).^2./T^2));
 end
